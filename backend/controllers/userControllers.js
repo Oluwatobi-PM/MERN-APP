7a4f5cbdc4ef70a07455505e7a9360661683e2e5
Oluwatobi-PM <rajioluwatobi3@gmail.com>
2022-06-16 15:35:19 +0100

    register

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,18 @@
+const asyncHandler = require('express-async-handler')
+
+const registerUser = asyncHandler (async (req,res) => {
+    const {name, email, password} = req.body
+    if(!name ||  !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+})
+
+const loginUser = asyncHandler (async (req,res) => {
+    res.send('login Route')
+})
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+const app = express()
+
+//Connect to databse
+connectDB()
+
+app.get('/', (req,res) => {
+    res.status(201).json({message: 'Welcome to the Support Desk API'})
+})
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
